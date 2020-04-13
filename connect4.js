54cf3c414185ea454090258cb28e6063e48d74a7
Lydia <snowpeech@gmail.com>
2020-04-13 12:03:40 -0500

    troubleshooting ...

--- connect4.js
+++ connect4.js
@@ -5,20 +5,19 @@
  * board fills (tie)
  */
 
-var WIDTH = 7;
-var HEIGHT = 6;
+let WIDTH = 7;
+let HEIGHT = 6;
 
-var currPlayer = 1; // active player: 1 or 2
-var board = []; // array of rows, each row is array of cells  (board[y][x])
+let currPlayer = 1; // active player: 1 or 2
+let board = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
  */
 
 function makeBoard() {
-  // TODO: set "board" to empty HEIGHT x WIDTH matrix array
   //create HEIGHT# of arrays with length of WIDTH (push HEIGHT times)
-  let row = new Array(WIDTH);
+  let row = Array.from({ length: WIDTH });
   for (let i = 0; i < HEIGHT; i++) {
     board.push(row);
   }
@@ -29,24 +28,24 @@ function makeBoard() {
 
 function makeHtmlBoard() {
   // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
-  let htmlBoard = document.querySelector("#board");
+  const htmlBoard = document.querySelector("#board");
   // TODO: add comment for this code
-  var top = document.createElement("tr"); //creates a table row element
+  const top = document.createElement("tr"); //creates a table row element
   top.setAttribute("id", "column-top"); // sets the ID of created row as column-top
   top.addEventListener("click", handleClick); //adds event listener to created row
 
-  for (var x = 0; x < WIDTH; x++) {
+  for (let x = 0; x < WIDTH; x++) {
     //
-    var headCell = document.createElement("td"); //create a cell for the row, this will fill out the width
+    const headCell = document.createElement("td"); //create a cell for the row, this will fill out the width
     headCell.setAttribute("id", x); // the id will be nums 0-WIDTH (6)
     top.append(headCell); //attach the cell
   }
   htmlBoard.append(top); //attaches the column-top row to the board
 
-  // TODO: add comment for this code
-  for (var y = 0; y < HEIGHT; y++) {
+  //make rest of the board
+  for (let y = 0; y < HEIGHT; y++) {
     const row = document.createElement("tr"); //create table rows for the height
-    for (var x = 0; x < WIDTH; x++) {
+    for (let x = 0; x < WIDTH; x++) {
       const cell = document.createElement("td"); //create a cell for the width
       cell.setAttribute("id", `${y}-${x}`); // the id will be "y-x" (Height index - width index)
       row.append(cell); //cell is appended to created row
@@ -58,68 +57,76 @@ function makeHtmlBoard() {
 /** findSpotForCol: given column x, return top empty y (null if filled) */
 
 function findSpotForCol(x) {
-  if (board[0][x] !== null) {
-    return null;
-  }
   for (let i = HEIGHT - 1; i >= 0; i--) {
-    if (board[i][x] === null) {
+    console.log("for loop", i);
+    if (!board[i][x]) {
+      console.log("if", i, x);
       return i;
     }
   }
+  return null;
 }
 
 /** placeInTable: update DOM to place piece into HTML table of board */
 
+// function placeInTable(y, x) {
+//   const marker = document.createElement("div");
+//   marker.classList.add("piece"); // may want to remove p1..
+//   piece.classList.add(`p${currPlayer}`);
+//   const position = document.getElementById(`${y}-${x}`);
+//   position.append(marker);
+// }
+/** placeInTable: update DOM to place piece into HTML table of board */
+
 function placeInTable(y, x) {
-  let marker = document.createElement("div");
-  marker.classList.add("piece", "p2"); // may want to remove p1..
-  let position = document.getElementById(`${y}-${x}`);
-  position.append(marker);
+  const piece = document.createElement("div");
+  piece.classList.add("piece");
+  piece.classList.add(`p${currPlayer}`);
+  // piece.style.top = -50 * (y + 2);
+
+  const spot = document.getElementById(`${y}-${x}`);
+  spot.append(piece);
 }
 
 /** endGame: announce game end */
 
 function endGame(msg) {
-  // TODO: pop up alert message
+  alert(`${msg}`);
 }
 
 /** handleClick: handle click of column top to play piece */
 
 function handleClick(evt) {
   // get x from ID of clicked cell
-  var x = +evt.target.id;
-  console.log("evt", evt, "evt+", +evt.target.id);
+  let x = +evt.target.id;
 
   // get next spot in column (if none, ignore click)
-  var y = findSpotForCol(x);
+  let y = findSpotForCol(x);
+  console.log("y", y);
   if (y === null) {
     return;
   }
 
-  // place piece in board and add to HTML table
-  // TODO: add line to update in-memory board
-  placeInTable(y, x);
-
   board[y][x] = currPlayer;
+  // place piece in board and add to HTML table
+  placeInTable(y, x);
+  // board[y][x] = currPlayer;
 
   // check for win
-  if (checkForWin()) {
-    return endGame(`Player ${currPlayer} won!`);
-  }
+  // if (checkForWin()) {
+  //   return endGame(`Player ${currPlayer} won!`);
+  // }
 
-  // check for tie
-  // TODO: check if all cells in board are filled; if so call, call endGame
-  if (
-    board
-      .join()
-      .split(",")
-      .every((val) => val == 1 || val == 2)
-  ) {
-    return endGame("It's a tie!");
-  }
+  // if (
+  //   board
+  //     .join()
+  //     .split(",")
+  //     .every((val) => val == 1 || val == 2)
+  // ) {
+  //   return endGame("It's a tie!");
+  // }
 
-  // switch players
-  // TODO: switch currPlayer 1 <-> 2
+  // switch players:
   currPlayer === 1 ? (currPlayer = 2) : (currPlayer = 1);
 }
 
@@ -127,9 +134,8 @@ function handleClick(evt) {
 
 function checkForWin() {
   function _win(cells) {
-    // Check four cells to see if they're all color of current player
-    //  - cells: list of four (y, x) cells
-    //  - returns true if all are legal coordinates & all match currPlayer
+    // creates array of 4 cells to check in each direction for every y and x value
+    //then runs those values to see if those are all the same as currPlayer.
 
     return cells.every(
       ([y, x]) =>
@@ -141,29 +147,27 @@ function checkForWin() {
     );
   }
 
-  // TODO: read and understand this code. Add comments to help you.
-
-  for (var y = 0; y < HEIGHT; y++) {
-    for (var x = 0; x < WIDTH; x++) {
-      var horiz = [
+  for (let y = 0; y < HEIGHT; y++) {
+    for (let x = 0; x < WIDTH; x++) {
+      const horiz = [
         [y, x],
         [y, x + 1],
         [y, x + 2],
         [y, x + 3],
       ];
-      var vert = [
+      const vert = [
         [y, x],
         [y + 1, x],
         [y + 2, x],
         [y + 3, x],
       ];
-      var diagDR = [
+      const diagDR = [
         [y, x],
         [y + 1, x + 1],
         [y + 2, x + 2],
         [y + 3, x + 3],
       ];
-      var diagDL = [
+      const diagDL = [
         [y, x],
         [y + 1, x - 1],
         [y + 2, x - 2],
